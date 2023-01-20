36ca2aa50e280ff3b863146b18f89932db3b24c6
aless133 <aless133@yandex.ru>
2023-01-21 02:52:35 +0300

    feat: implement draw

---
+++ src/backend/draw.ts
@@ -0,0 +1,63 @@
+import { Writable, Readable } from "node:stream";
+import { mouse, left, right, up, down, straightTo, Button, Point } from "@nut-tree/nut-js";
+
+const readableStream = new Readable({
+  read: () => {
+    /*empty!*/
+  },
+  objectMode: true,
+});
+
+let parentStream: Writable;
+
+const connect = (parent: Writable) => {
+  parentStream = parent;
+  return readableStream;
+};
+
+export default connect;
+
+readableStream.on("data", async (data) => {
+  console.log("draw", "readableStream.on", data);
+  if (data.cmd === "draw_square") {
+    await drawRect(+data.arg1, +data.arg1);
+  } else if (data.cmd === "draw_rectangle") {
+    await drawRect(+data.arg1, +data.arg2);
+  } else if (data.cmd === "draw_circle") {
+    await drawCircle(+data.arg1);
+  }
+});
+
+const drawRect = async (a: number, b: number) => {
+  // const start = await mouse.getPosition();
+
+  await mouse.pressButton(Button.LEFT);
+
+  // await mouse.move(straightTo(new Point(start.x + a, start.y)));
+  // await mouse.move(straightTo(new Point(start.x + a, start.y + b)));
+  // await mouse.move(straightTo(new Point(start.x, start.y + b)));
+  // await mouse.move(straightTo(new Point(start.x, start.y)));
+
+  await mouse.move(right(a));
+  await mouse.move(down(b));
+  await mouse.move(left(a));
+  await mouse.move(up(b));
+
+  await mouse.releaseButton(Button.LEFT);
+};
+
+const drawCircle = async (r: number) => {
+  const start = await mouse.getPosition();
+  const center = new Point(start.x, start.y + r);
+  const step = 1 / r;
+
+  await mouse.pressButton(Button.LEFT);
+
+  for (let rad = 0; rad <= Math.PI * 2; rad += step) {
+    const y = r * Math.cos(rad);
+    const x = r * Math.sin(rad);
+    await mouse.move(straightTo(new Point(center.x + x, center.y - y)));
+  }
+
+  await mouse.releaseButton(Button.LEFT);
+};
--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,6 +1,7 @@
 import { Writable } from "node:stream";
 import { WebSocket, WebSocketServer } from "ws";
 import connectNav from "./nav";
+import connectDraw from "./draw";
 
 export const backend = (port: number) => {
   console.log("Hello! I'm backend on", port);
@@ -20,12 +21,15 @@ export const backend = (port: number) => {
     });
 
     const nav = connectNav(answers);
+    const draw = connectDraw(answers);
 
     ws.on("message", (message: Buffer) => {
       console.log("\nws.Received message", message.toString());
       const cmd = message.toString().split(" ");
       if (["mouse_up", "mouse_down", "mouse_left", "mouse_right", "mouse_position"].includes(cmd[0])) {
         nav.push({ cmd: cmd[0], arg: cmd[1] });
+      } else if (["draw_square", "draw_rectangle", "draw_circle"].includes(cmd[0])) {
+        draw.push({ cmd: cmd[0], arg1: cmd[1], arg2: cmd[2] });
       }
     });
   });
